3a528f6aef58bb172384d6e3dd769b32c7e3586b
Mathieu <ve2tmq@gmail.com>
2025-12-16 07:10:13 -0500

    White message in English

--- background.js
+++ background.js
@@ -1,16 +1,13 @@
-// background.js - Version FIDO2 Ultra-Légère
+// background.js
 
-// 1. On crée l'entrée dans le menu clic-droit au démarrage
 browser.contextMenus.create({
   id: "inject-signature",
-  title: "Signer avec Clé de Sécurité (FIDO2)",
-  contexts: ["all"] // On laisse "all" pour être sûr qu'il apparaisse partout
+  title: "Sign with key (FIDO2)",
+  contexts: ["all"]
 });
 
-// 2. On écoute le clic sur ce menu
 browser.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "inject-signature") {
-    // Le SEUL job du background : dire au content.js "C'est à toi de jouer !"
     browser.tabs.sendMessage(tab.id, { action: "injectSignature" });
   }
 });
